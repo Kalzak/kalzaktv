0b6c48981bae0dfc725ce6f9253d218b7f92be85
Kalzak <kalzak.dev@protonmail.com>
2023-10-26 12:43:28 +0800

    tab bar now flashes (((:

--- src/App.js
+++ src/App.js
@@ -1,61 +1,84 @@
 import './App.css';
+import ContactButton from './ContactButton';
+import { Helmet } from 'react-helmet';
+import React, { useState, useEffect } from 'react';
 
 import avatarGif from "./assets/avatar_static_loop.gif";
 import contacts from "./data/contacts.json";
 import staticVideo from "./assets/static.mp4";
 
-function App() {
-  return (
 
-    <div className="w-full h-[calc(100dvh)] bg-tvwhite-0 flex justify-center items-center">
 
+
+function ContactPage() {
+    /*
+    const [color, setColor] = useState("#2dba14");
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+    */
+    const colors = [
+        "#81EBEB",
+        "#7A7AE5",
+        "#E561C7",
+        "#55F255",
+        "#E25050",
+    ]
+
+    const [colorIndex, setColorIndex] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
     
-      <video position="absolute" autoplay="autoplay" muted="muted" loop="loop" style={{ width: "100vw", height: "100vh", opacity: "30%", position: "absolute", zIndex: "-1", filter: "invert(1)", objectFit: "cover" }}>
-        <source src={staticVideo} type="video/mp4" />
-      </video>
+    return (
 
-      <div className="max-w-2xl mx-auto flex flex-col gap-2 sm:gap-5">
+        <div className="w-full h-[calc(100dvh)] bg-tvwhite-0 flex justify-center items-center transition-height duration-300 ease-in-out">
+            <Helmet>
+                <meta name="theme-color" content={colors[colorIndex]} />
+            </Helmet>
+            <video class="video-background" position="absolute" autoplay="autoplay" muted="muted" loop="loop" playsInline style={{ width: "100vw", height: "100vh", opacity: "30%", position: "absolute", zIndex: "-1", filter: "invert(1)", objectFit: "cover" }}>
 
-        <div className="h-40 w-40 sm:h-56 sm:w-56 mx-auto">
-          <div className="aspect-w-1 aspect-h-1">
-            <img src={avatarGif} className="rounded-full object-cover object-center border-4 border-black" />
-          </div>
-        </div>
+                <source src={staticVideo} type="video/mp4" />
+            </video>
 
-        <div className="text-center sm:p-3">
-          <h1 className="text-4xl font-bold">Kalzak</h1>
-          <p className="text-2xl sm:mt-3">
-            Security Researcher
-          </p>
-        </div>
+            <div className="max-w-2xl mx-auto flex flex-col gap-2 sm:gap-5">
 
-        <div className="flex flex-col gap-5 sm:gap-10">
-          {contacts.map(({ text, username, link, color, textcolor }, index) => {
-            console.log(index, text, username, link, color, textcolor);
-
-            let target = "_blank";
-            if (link == "#") {
-              target = "";
-              link = `javascript:alert(\"${text} doesn't support url invites, add me directly\")`;
-            }
-
-            return (
-              <a href={link} key={index} target={target}>
-                <div className={`w-80 sm:w-96 mx-auto ${color} 
-                text-center text-xl font-bold py-2.5 sm:py-3
-                border-2 border-black
-                shadow-custom hover:shadow-none transition-all
-                hover:translate-x-1 hover:translate-y-1`}>
-                  <h1>{text}: {username}</h1>
+                <div className="h-40 w-40 sm:h-56 sm:w-56 mx-auto">
+                    <div className="aspect-w-1 aspect-h-1">
+                        <img src={avatarGif} className="rounded-full object-cover object-center border-4 border-black" />
+                    </div>
                 </div>
-              </a>
-            );
-          })}
-        </div>
-      </div>
-    </div>
 
-  );
+                <div className="text-center sm:p-3">
+                    <h1 className="text-4xl font-bold">Kalzak</h1>
+                    <p className="text-2xl sm:mt-3">
+                        Security Researcher
+                    </p>
+                </div>
+
+                <div className="flex flex-col gap-5 sm:gap-10">
+                    {contacts.map(({ text, username, link, color}) => {
+                        return (
+                            <ContactButton text={text} username={username} link={link} color={color}/>
+                        );
+                    })}
+                </div>
+
+            </div>
+
+        </div>
+
+    );
 }
 
-export default App;
+export default ContactPage;
---
+++ src/ContactButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function ContactButton(props) {
+    let target = "_blank";
+    let link = props.link;
+    if (props.link === "#") {
+        target = "";
+        link = `javascript:alert("${props.text} doesn't support url invites, add me directly")`;
+    }
+
+    return (
+        <a href={link} key={props.index} target={target}>
+            <div className={`w-80 sm:w-96 mx-auto ${props.color} 
+            text-center text-xl font-bold py-2.5 sm:py-3
+            border-2 border-black
+            shadow-custom hover:shadow-none transition-all
+            hover:translate-x-1 hover:translate-y-1`}>
+                <h1>{props.text}: {props.username}</h1>
+            </div>
+        </a>
+    );
+}
+
+export default ContactButton;
